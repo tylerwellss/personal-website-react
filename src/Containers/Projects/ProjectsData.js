4d0e871dbe16567741823f99b8403c42a52e8ec2
Tyler Wells <tyler.wellss@gmail.com>
2018-12-25 00:58:29 +0800

    reword some personal info, remove unused file

--- src/Containers/Projects/ProjectsData.js
+++ src/Containers/Projects/ProjectsData.js
@@ -11,7 +11,7 @@ const ProjectsData = {
     "title": "URL Shortener",
     "desc": "Input a long URL and receive a short URL to use instead. Sign in to save your shortened URLs.",
     "techUsed": "React, MongoDB, Firebase, NodeJS, Express",
-    "codeLink": "http://www.github.com/wellstyler/url-shortener",
+    "codeLink": "http://www.github.com/therealtylerwells/url-shortener",
     "liveLink": "https://url-shortener-123456789.firebaseapp.com/",
     "imageUrl": "https://res.cloudinary.com/asdfbot/image/upload/v1530170970/preview-url-shortener_lyucxf.png",
     "imageAltText": "URL Shortener Website Preview"
@@ -20,7 +20,7 @@ const ProjectsData = {
     "title": "Crypto Tracker",
     "desc": "Track your cryptocurrency losses. Sign in, save/update your portfolio, and check current prices.",
     "techUsed": "React, React Redux, Redux Thunk, MongoDB, Firebase, NodeJS, Express",
-    "codeLink": "https://github.com/wellstyler/crypto-tracker",
+    "codeLink": "https://github.com/therealtylerwells/crypto-tracker",
     "liveLink": "https://track-my-crypto.firebaseapp.com/",
     "imageUrl": "https://res.cloudinary.com/asdfbot/image/upload/v1530170970/preview-crypto-tracker_ggjs0o.png",
     "imageAltText": "URL Shortener Website Preview"
@@ -29,8 +29,8 @@ const ProjectsData = {
     "title": "Reddit Translation Bot",
     "desc": "A Reddit bot that translates Chinese to English and generates Pinyin in Chinese-focused subreddits",
     "techUsed": "NodeJS, Snoostorm, Snoowrap, Baidu Translate",
-    "liveLink": "http://https://www.reddit.com/user/chinese_to_english",
-    "codeLink": "https://github.com/wellstyler/reddit-bot",
+    "liveLink": "https://www.reddit.com/user/chinese_to_english",
+    "codeLink": "https://github.com/therealtylerwells/reddit-bot",
     "imageUrl": "https://res.cloudinary.com/asdfbot/image/upload/v1531813640/preview_reddit_bot_boucnq.png",
     "imageAltText": "Reddit Bot Project Preview"
   },
@@ -38,7 +38,7 @@ const ProjectsData = {
     "title": "Hyperledger Starter",
     "desc": "Starter repo for blockchain projects w/ Hyperledger Fabric, Composer, and Explorer.",
     "techUsed": "Hyperledger Fabric, Hyperledger Composer, Hyperledger Explorer",
-    "codeLink": "https://github.com/wellstyler/hyperledger-starter",
+    "codeLink": "https://github.com/therealtylerwells/hyperledger-starter",
     "imageUrl": "https://res.cloudinary.com/asdfbot/image/upload/v1530173438/preview-hyperledger-starter_an6dey.png",
     "imageAltText": "Hyperledger Starter Repository Preview"
   },
@@ -46,7 +46,7 @@ const ProjectsData = {
     "title": "Web Scraper",
     "desc": "A sample web scraper. Scrape a job ad from my website and automate inputting that data to editpad.org.",
     "techUsed": "Headless Chrome, Puppeteer, Optimist, NodeJS",
-    "codeLink": "https://github.com/wellstyler/nodejs-puppeteer-web-scrape",
+    "codeLink": "https://github.com/therealtylerwells/nodejs-puppeteer-web-scrape",
     "imageUrl": "https://res.cloudinary.com/asdfbot/image/upload/v1530174394/preview-web-scraper_qhloo9.png",
     "imageAltText": "Web Scraper Repository Preview"
   },
@@ -54,7 +54,7 @@ const ProjectsData = {
     "title": "Personal Website",
     "desc": "The website you're looking at. Made with React, styled with Material-UI, hosted on Github Pages.",
     "techUsed": "React, Github Pages, Material-UI Styling",
-    "codeLink": "https://github.com/wellstyler/personal-website-react",
+    "codeLink": "https://github.com/therealtylerwells/personal-website-react",
     "liveLink": "https://www.tylerwells.net",
     "imageUrl": "https://res.cloudinary.com/asdfbot/image/upload/v1530174960/preview-personal-website_pkakut.png",
     "imageAltText": "Personal Website Preview"
@@ -64,7 +64,7 @@ const ProjectsData = {
     "desc": "A demo to build a burger, order it, and view your previous orders. From Udemy tutorial.",
     "techUsed": "React, React Redux, Redux Thunk, Firebase, Udemy",
     "liveLink": "https://react-my-burger-d192e.firebaseapp.com/",
-    "codeLink": "https://github.com/wellstyler/react-burger-builder",
+    "codeLink": "https://github.com/therealtylerwells/react-burger-builder",
     "imageUrl": "https://res.cloudinary.com/asdfbot/image/upload/v1530174960/preview-burger-builder_xkngmq.png",
     "imageAltText": "React Burger Builder Website Preview"
   }
