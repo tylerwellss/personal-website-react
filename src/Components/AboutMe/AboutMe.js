785b7d33a2b17e17d55e8f1be102aa39d9cf683d
Tyler Wells <tyler.wellss@gmail.com>
2019-05-15 18:16:53 -0500

    update about me text

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -7,11 +7,8 @@ const AboutMe = () => {
   return (
     <div className="AboutMe">
       <h2>About me</h2>
-      <p>Hello. I'm an American software engineer currently living in Shanghai, China. I work on the <a href="https://www.pwc.com/us/en/increasing-it-effectiveness/emerging-technology-lab.html" target="_blank" rel="noopener noreferrer">Emerging Tech team</a> at 
-      PwC. We build PoCs and enterprise solutions using new technology to illustrate how new technology can 
-      benefit businesses. Lately, I'm building new solutions to old problems using blockchain platforms 
-      like <a href="https://www.hyperledger.org/" target="_blank" rel="noopener noreferrer">Hyperledger</a> and <a href="https://www.ethereum.org/" target="_blank" rel="noopener noreferrer">Ethereum</a>.  
-      I like working with Javascript, especially React, React Native, and NodeJS.      
+      <p>Hey, I'm Tyler. I'm a software engineer living in Kansas City.
+      I like working with Javascript, especially React, React Native, and NodeJS.
       You can find my 
       contact information on this page, as well as a list of <Link to={routes.PROJECTS}>projects</Link> including some details, a link to the code, and a link to the live project. 
       Feel free to <a href="mailto:tyler.wellss@gmail.com">get in touch</a> if you'd like to chat.</p>
