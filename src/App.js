617a940972994d518ecbbaaef1817ac305793167
Tyler Wells <13863967+therealtylerwells@users.noreply.github.com>
2021-09-12 11:54:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navigation from './Components/Navigation/Navigation'
-import * as routes from './Constants/routes';
-import Home from './Containers/Home/Home';
-import Projects from './Containers/Projects/Projects';
-// import Footer from './Components/Footer/Footer';
 
 class App extends Component {
   render() {
     return (
-      /**
-      <Router>
-        <div>
-          <Navigation />
-          <Route exact path={routes.HOME}
-            component={() => <Home />} />
-          <Route path={routes.PROJECTS}
-            component={() => <Projects />}
-          />
-        </div>
-      </Router>
-      **/
       <p>What are you doing here? If you need to contact me, you can: tyler.wellss at gmail dot com</p>
     );
   }
